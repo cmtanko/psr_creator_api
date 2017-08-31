59074d7698aa16b10fc263d6a40371f57db71bd9
Suchan Badyakar <suchanbadyakar@lftechnology.com>
2017-08-31 13:42:07 +0545

    PSRG-008 -m Code cleanups for report generator -t 2 hr -s Completed

--- dist/report/report.controller.js
+++ dist/report/report.controller.js
@@ -17,8 +17,23 @@ var _lodash2 = _interopRequireDefault(_lodash);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var router = (0, _express.Router)();
+
+router.post('/', function (req, res, next) {
+  var query = req.body;
+  var assigneeString = getAssigneeString(query.assignee);
+  var assigneeArray = query.assignee.split(',');
+  _axios2.default.get(query.url + '/rest/api/latest/search?jql=' + assigneeString + '&maxResults=' + 100 * assigneeArray.length, {
+    method: 'GET',
+    headers: { 'Authorization': 'Basic ' + query.token }
+  }).then(function (data) {
+    var results = composeTaskResult(_lodash2.default.get(data, 'data.issues'), query);
+    res.send({ 'result': results });
+  }).catch(function (data) {
+    res.send({ 'error': 'Unable to fetch data!' + data });
+  });
+});
+
 var getStatus = function getStatus(statusCode, query) {
-  console.log(statusCode + '---' + query);
   if (statusCode.toLowerCase() === query.inprogress.toLowerCase()) {
     return 'In Progress';
   } else if (statusCode.toLowerCase() === query.completed.toLowerCase()) {
@@ -29,10 +44,10 @@ var getStatus = function getStatus(statusCode, query) {
   return statusCode;
 };
 
-router.post('/', function (req, res, next) {
-  var query = req.body;
+var getAssigneeString = function getAssigneeString(assignee) {
+  //CONCATENATE ALL ASSIGNEES IF MORE THAN ONE WITH CORRECT FORMAT
   var assigneeString = "";
-  var assigneeArray = query.assignee.split(',');
+  var assigneeArray = assignee.split(',');
   assigneeArray.forEach(function (a, i, array) {
     if (i === array.length - 1) {
       assigneeString += 'assignee=' + a;
@@ -40,33 +55,27 @@ router.post('/', function (req, res, next) {
       assigneeString += 'assignee=' + a + '||';
     }
   }, assigneeString);
-  console.log(assigneeString);
-  _axios2.default.get(query.url + '/rest/api/latest/search?jql=' + assigneeString + '&maxResults=' + 100 * assigneeArray.length, {
-    method: 'GET',
-    headers: { 'Authorization': 'Basic ' + query.token }
-  }).then(function (data) {
-    var results = data.data.issues;
-    var issues = [];
-    _lodash2.default.each(results, function (result) {
-      if (_lodash2.default.get(result, 'fields.status.name') !== 'Backlog') {
-        issues.push({
-          task_id: result.key,
-          task_type: _lodash2.default.get(result, 'fields.issuetype.name'),
-          task_description: _lodash2.default.get(result, 'fields.summary'),
-          task_creation_date: _lodash2.default.get(result, 'fields.created'),
-          task_updated_date: _lodash2.default.get(result, 'fields.updated'),
-          task_assignee: _lodash2.default.get(result, 'fields.assignee.displayName'),
-          task_project: _lodash2.default.get(result, 'fields.project.key'),
-          task_status: getStatus(_lodash2.default.get(result, 'fields.status.name'), query)
-        });
-      }
-    }, query);
-    results = _lodash2.default.sortBy(issues, ['task_status', 'task_id']);
-    res.send({ 'result': results });
-  }).catch(function (data) {
-    res.send({ 'error': 'Unable to fetch data!' + data });
-  });
-});
+  return assigneeString;
+};
+
+var composeTaskResult = function composeTaskResult(results, query) {
+  var issues = [];
+  _lodash2.default.each(results, function (result) {
+    if (_lodash2.default.get(result, 'fields.status.name') !== 'Backlog') {
+      issues.push({
+        task_id: result.key,
+        task_type: _lodash2.default.get(result, 'fields.issuetype.name'),
+        task_description: _lodash2.default.get(result, 'fields.summary'),
+        task_creation_date: _lodash2.default.get(result, 'fields.created'),
+        task_updated_date: _lodash2.default.get(result, 'fields.updated'),
+        task_assignee: _lodash2.default.get(result, 'fields.assignee.displayName'),
+        task_project: _lodash2.default.get(result, 'fields.project.key'),
+        task_status: getStatus(_lodash2.default.get(result, 'fields.status.name'), query)
+      });
+    }
+  }, query);
+  return _lodash2.default.sortBy(issues, ['task_status', 'task_id']);
+};
 
 exports.default = router;
 //# sourceMappingURL=report.controller.js.map
--- src/report/report.controller.js
+++ src/report/report.controller.js
@@ -3,18 +3,36 @@ import axios from 'axios';
 import _ from 'lodash';
 
 const router = Router();
+
+router.post('/', (req, res, next) => {
+  let query = req.body;
+  let assigneeString = getAssigneeString(query.assignee);
+  let assigneeArray = query.assignee.split(',');
+  axios.get(query.url + '/rest/api/latest/search?jql=' + assigneeString + '&maxResults=' + 100 * assigneeArray.length,
+    {
+      method: 'GET',
+      headers: { 'Authorization': 'Basic ' + query.token }
+    }
+  ).then((data) => {
+    let results = composeTaskResult(_.get(data, 'data.issues'), query);
+    res.send({ 'result': results });
+  })
+    .catch((data) => {
+      res.send({ 'error': 'Unable to fetch data!' + data });
+    });
+});
+
 const getStatus = (statusCode, query) => {
-  console.log(statusCode + '---' + query);
   if (statusCode.toLowerCase() === query.inprogress.toLowerCase()) { return 'In Progress'; }
   else if (statusCode.toLowerCase() === query.completed.toLowerCase()) { return 'Completed'; }
   else if (statusCode.toLowerCase() === query.todo.toLowerCase()) { return 'To Do'; }
   return statusCode;
 };
 
-router.post('/', (req, res, next) => {
-  let query = req.body;
+const getAssigneeString = (assignee) => {
+  //CONCATENATE ALL ASSIGNEES IF MORE THAN ONE WITH CORRECT FORMAT
   let assigneeString = "";
-  let assigneeArray = query.assignee.split(',');
+  let assigneeArray = assignee.split(',');
   assigneeArray.forEach(function (a, i, array) {
     if (i === array.length - 1) {
       assigneeString += 'assignee=' + a;
@@ -22,35 +40,27 @@ router.post('/', (req, res, next) => {
       assigneeString += 'assignee=' + a + '||';
     }
   }, assigneeString);
-  console.log(assigneeString);
-  axios.get(query.url + '/rest/api/latest/search?jql=' + assigneeString + '&maxResults=' + 100 * assigneeArray.length,
-    {
-      method: 'GET',
-      headers: { 'Authorization': 'Basic ' + query.token }
+  return assigneeString;
+}
+
+const composeTaskResult = (results, query) => {
+  let issues = [];
+  _.each(results, (result) => {
+    if (_.get(result, 'fields.status.name') !== 'Backlog') {
+      issues.push({
+        task_id: result.key,
+        task_type: _.get(result, 'fields.issuetype.name'),
+        task_description: _.get(result, 'fields.summary'),
+        task_creation_date: _.get(result, 'fields.created'),
+        task_updated_date: _.get(result, 'fields.updated'),
+        task_assignee: _.get(result, 'fields.assignee.displayName'),
+        task_project: _.get(result, 'fields.project.key'),
+        task_status: getStatus(_.get(result, 'fields.status.name'), query)
+      });
     }
-  ).then((data) => {
-    let results = data.data.issues;
-    let issues = [];
-    _.each(results, (result) => {
-      if (_.get(result, 'fields.status.name') !== 'Backlog') {
-        issues.push({
-          task_id: result.key,
-          task_type: _.get(result, 'fields.issuetype.name'),
-          task_description: _.get(result, 'fields.summary'),
-          task_creation_date: _.get(result, 'fields.created'),
-          task_updated_date: _.get(result, 'fields.updated'),
-          task_assignee: _.get(result, 'fields.assignee.displayName'),
-          task_project: _.get(result, 'fields.project.key'),
-          task_status: getStatus(_.get(result, 'fields.status.name'), query)
-        });
-      }
-    }, query);
-    results = _.sortBy(issues, ['task_status', 'task_id']);
-    res.send({ 'result': results });
-  })
-    .catch((data) => {
-      res.send({ 'error': 'Unable to fetch data!' + data });
-    });
-});
+  }, query);
+  return _.sortBy(issues, ['task_status', 'task_id']);
+}
+
 
 export default router;
